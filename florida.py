dc31d5499f65f528c9731242832fb338f4747ea5
Harold Hannon <haroldhannon@hotmail.com>
2018-11-20 23:21:50 -0600

    Update florida.py

--- florida.py
+++ florida.py
@@ -9,36 +9,65 @@ app = Flask(__name__)
 def rreplace(s, old, new, count):
     return (s[::-1].replace(old[::-1], new[::-1], count))[::-1]
 
-
-def get_florida_string():
+def get_pod_list():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
 
-    #this probably todo needs an environment variable for the pod selector
-    podlist = v1.list_namespaced_pod("default", label_selector='app=dynomite,role=worker')
+    # this probably todo needs an environment variable for the pod selector
+    return v1.list_namespaced_pod("default", label_selector='app=dynomite,role=worker')
 
+def parse_item(item):
+    podip = item.status.pod_ip
+    generatename = item.metadata.generate_name
+    name = item.metadata.name
+    token = item.spec.containers[0].env[0].value
+    token = token.replace("'", "")
+    name = name.replace(generatename, "")
+    rackname = "us-east-" + name
+    # most likely todo need a namespace for rack and search for labelled port?
+    items = [podip, rackname, token]
+    return items
+
+
+def get_florida_string():
+    podlist = get_pod_list()
     floridastring = ""
     for item in podlist.items:
-        podip = item.status.pod_ip
-        generatename = item.metadata.generate_name
-        name = item.metadata.name
-        token = item.spec.containers[0].env[0].value
-        token = token.replace("'", "")
-        name = name.replace(generatename, "")
-        rackname = "us-east-" + name
-        #most likely todo need a namespace for rack and search for labelled port?
-        florida = podip.strip() + ":8101:" + rackname.strip() + ":dc1:" + token.strip()
+        items = parse_item(item)
+        florida = items[0].strip() + ":8101:" + items[1].strip() + ":dc1:" + items[2].strip()
         floridastring = floridastring + florida + "|"
 
     floridastring = rreplace(floridastring, "|", "", 1)
     return floridastring
+
+def get_conductor_string():
+    podlist = get_pod_list()
+    conductorstring = ""
+    filestring = ""
+    for item in podlist.items:
+        items = parse_item(item)
+        # most likely todo need a namespace for rack and search for labelled port?
+        conductor = items[0].strip() + ":8102:" + items[1].strip()
+        #host1:8102:us-east-1c;
+        conductorstring = conductorstring + conductor + ";"
+
+    conductorstring = rreplace(conductorstring, ";", "", 1)
+
+    with open('config.properties', 'r') as myfile:
+        filestring = myfile.read().replace('REPLACEME', conductorstring)
+
+    return filestring
 
 
 @app.route('/REST/v1/admin/get_seeds')
 def florida():
     return get_florida_string()
 
+
+@app.route('/REST/v1/admin/config.properties')
+def conductor():
+    return get_conductor_string()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
